fc9a7c9b87581e14dc1f4d417ff70c177786b33e
tharindulakmal651 <155797706+tharindulakmal651@users.noreply.github.com>
2026-05-19 22:59:11 +0530

    usestate and route

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -2,44 +2,28 @@ import { useState } from "react"
 //import "./testing.css"
 
 export default function Testing() {
-  // const [count, setCount] = useState(0)
-   const [count_1,setCount_1]  = useState(0)
-   const [name,setName] = useState("Tharindu")
+  
+   const [count,setCount]  = useState(0)
+   const [name,setName] = useState("Student")
    
-  /* function increment() {
-    if(count < 10) {
-        setCount(count + 1)
-        console.log(count + 1)
-    }
-   }
+ 
 
-
-    function decrement() {
-     if(count > 0) {
-         setCount(count - 1)
-         console.log(count - 1)
-     }   
-}
-*/
-
-   function increment() {
-    if(count_1 <100 ){   
-       setCount_1(count_1 + 10)
-       console.log(count_1 + 10)
+    function increment() {
+    if(count <100 ){   
+       setCount(count + 10)
+       
    }
   }
 
     function decrement() {
-       if(count_1 >0 ){
-       setCount_1(count_1 - 10)
-       console.log(count_1 - 10)
+       if(count >0 ){
+       setCount(count - 10)
+       
    }
   }
  
  
-
-
-   function changeName(value) {
+    function changeName(value) {
      setName(value)
    }
 
@@ -48,7 +32,7 @@ export default function Testing() {
       <h1>{name}</h1>
 
       <button className="val" onClick={decrement}> - </button>
-      <span> {count_1}</span>
+      <span> {count}</span>
       <button className="val" onClick={increment}> + </button>
     
       <div className="button-panel">
